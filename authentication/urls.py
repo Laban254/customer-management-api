9f0e56b07518543a5e183b361e174bd126b708d9
laban254 <labanrotich6544@gmail.com>
2024-09-23 22:01:26 +0300

    Add API Documentation with drf-spectacular

--- authentication/urls.py
+++ authentication/urls.py
@@ -7,9 +7,9 @@ from .views import  GoogleCallbackView, GoogleLoginView
 
 
 urlpatterns = [
-    path('auth/google/login/', GoogleLoginView.as_view(), name='google-login'),
-    path('auth/google/callback/', GoogleCallbackView.as_view(), name='google-callback'),
-    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('google/login/', GoogleLoginView.as_view(), name='google-login'),
+    path('google/callback/', GoogleCallbackView.as_view(), name='google-callback'),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
 ]
