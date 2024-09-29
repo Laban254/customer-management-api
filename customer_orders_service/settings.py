9fedb2dc414187cfe5269a9d5c1c650a4626897d
laban254 <labanrotich6544@gmail.com>
2024-09-29 19:29:42 +0300

    update logs dir creation

--- customer_orders_service/settings.py
+++ customer_orders_service/settings.py
@@ -20,9 +20,7 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-LOGS_DIR = os.path.join(BASE_DIR, 'logs')
-if not os.path.exists(LOGS_DIR):
-    os.makedirs(LOGS_DIR)
+LOGS_DIR = os.path.join(BASE_DIR, 'logs'); os.makedirs(LOGS_DIR, exist_ok=True)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
